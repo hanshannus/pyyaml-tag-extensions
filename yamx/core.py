b46d5d5c6732dfaef49b4a35213a210955fe6b5c
Johannes Hecher <hhecher@gmx.at>
2023-03-13 13:38:25 +0100

    FIX: remove Omegaconf since it is not able to handle python objects

--- yamx/core.py
+++ yamx/core.py
@@ -1,30 +1,26 @@
 from pathlib import Path
 import yaml
 import io
-from omegaconf import DictConfig, ListConfig
 from typing import IO, Union
 
 # library imports
 from .loaders import ExtendedLoader
 
-DEFAULT_OUTPUT: str = "primitive"  # {"omegaconf", "primitive"}
 DEFAULT_ROOT: str = "global"  # {"global", "relative"}
 
 
 def load(
     filepath: str,
     root: str = DEFAULT_ROOT,
-    output: str = DEFAULT_OUTPUT,
-) -> Union[DictConfig, ListConfig, dict, list]:
-    return Yaml.load(filepath, root=root, output=output)
+) -> Union[dict, list]:
+    return Yaml.load(filepath, root=root)
 
 
 def loads(
     stream: Union[str, IO],
     root: str = DEFAULT_ROOT,
-    output: str = DEFAULT_OUTPUT,
-) -> Union[DictConfig, ListConfig, dict, list]:
-    return Yaml.loads(stream, root=root, output=output)
+) -> Union[dict, list]:
+    return Yaml.loads(stream, root=root)
 
 
 class Yaml(ExtendedLoader):
@@ -38,18 +34,16 @@ class Yaml(ExtendedLoader):
         cls,
         filepath: Union[str, Path],
         root: Union[str, Path] = DEFAULT_ROOT,
-        output: str = DEFAULT_OUTPUT,
-    ) -> Union[DictConfig, ListConfig, dict, list]:
+    ) -> Union[dict, list]:
         with Path(filepath).open() as stream:
-            return cls.loads(stream, root=root, output=output)
+            return cls.loads(stream, root=root)
 
     @classmethod
     def loads(
         cls,
         stream: Union[str, IO],
         root: Union[str, Path] = DEFAULT_ROOT,
-        output: str = DEFAULT_OUTPUT,
-    ) -> Union[DictConfig, ListConfig, dict, list]:
+    ) -> Union[dict, list]:
         if isinstance(root, str) and root == "global":
             if isinstance(stream, IO):
                 cls.root_dir = Path(stream.name).resolve().parent
@@ -62,13 +56,4 @@ class Yaml(ExtendedLoader):
             stream = io.StringIO(stream)
         new_loader = ExtendedLoader
         new_loader.root_dir = cls.root_dir
-        res = yaml.load(stream, Loader=new_loader)
-        if not isinstance(res, (dict, list)):
-            raise ValueError(f"Unsupported type: {type(res)}")
-        if output == "omegaconf":
-            if isinstance(res, dict):
-                return DictConfig(res)
-            elif isinstance(res, list):
-                return ListConfig(res)
-        else:
-            return res
+        return yaml.load(stream, Loader=new_loader)
